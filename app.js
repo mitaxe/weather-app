80fe08141929d28663c83658330ef30c8a615984
BogdanNosovytskyy <b.nosovytskyy@gmail.com>
2018-05-30 17:49:28 +0300

    remove logs

--- app.js
+++ app.js
@@ -3,22 +3,12 @@ const hbs = require('hbs');
 const config = require('./config');
 const EOL = require('os').EOL;
 
-const fs = require('fs');
-
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getFullYear', () => new Date().getFullYear());
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) => {
-  const now = new Date().toString();
-  const log = `${now}, ${req.method}, ${req.url}${EOL}`;
-  fs.appendFile(config.logFile, log, (err) => {
-    if (err) throw err;
-  })
-  next();
-})
 
 app.get('/', (req, res) => {
   res.render('index.hbs', {
